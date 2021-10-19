39dc59be8692b2eb58bc16704d66bbdaa8851f4b
Mhnd <mrmohandtv@gmail.com>
2021-10-20 00:11:15 +0300

    Add .gitignore and .gitattributes.

---
+++ MathLibrary.Test/OperationTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MathLibrary.Test
+{
+    public class OperationTests
+    {
+        [Theory]
+        [InlineData(2,2,4)]
+        [InlineData(2, 5, 7)]
+        [InlineData(10, 2, 12)]
+        [InlineData(200, 2, 202)]
+        public void AdditionTest(int number1, int number2, int expected)
+        {
+            Operations operations = new Operations();
+
+            int actual = operations.Addition(number1, number2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubstractionTest()
+        {
+            Operations operations = new Operations();
+            int expected = 0;
+
+            int actual = operations.Substraction(2, 2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MultipluctonTest()
+        {
+            Operations operations = new Operations();
+            int expected = 2;
+
+            int actual = operations.Multipluction(1, 2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DivitionTest()
+        {
+            Operations operations = new Operations();
+            double expected = 2;
+
+            double actual = operations.Divition(4, 2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ MathLibrary/Operations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLibrary
+{
+    public class Operations
+    {
+        public int Addition(int number1, int number2)
+        {
+            int output = 0;
+
+            output = number2 + number1;
+
+            return output;
+        }
+
+        public int Substraction(int number1, int number2)
+        {
+            int output = 0;
+
+            output = number1 - number2;
+
+            return output;
+        }
+
+        public int Multipluction(int number1, int number2)
+        {
+            int output = 0;
+
+            output = number2 * number1;
+
+            return output;
+        }
+        public double Divition(double number1, double number2)
+        {
+            double output = 0;
+
+            output = number1 / number2;
+
+            return output;
+        }
+    }
+}
